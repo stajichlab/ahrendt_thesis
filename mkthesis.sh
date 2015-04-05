ba070d11607be1eef4e3e3f165646f253807bbf2
Steven Ahrendt <sahrendt0@gmail.com>
2015-04-05 08:58:09 -0700

    Script to run 'pdflatex' and 'bibtex' commands

--- mkthesis.sh
+++ mkthesis.sh
@@ -1,4 +1,5 @@
 # Run R CMD Sweave in "./Chapter_Inhibition/tab/" on "Ahrendt_thesis_ChInhibition_Tabs.Rnw"
+# Run R CMD Sweave in "./Chapter_RhodStruct/tab/" on "Ahrendt_thesis_ChRhodStruct_Tabs.Rnw"
 
 TEX="Ahrendt_thesis.tex"
 pdflatex $TEX
