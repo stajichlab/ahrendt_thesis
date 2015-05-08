c2a50d9221da2fdf5e360bf32f83b8b5ca5f329b
Steven Ahrendt <sahrendt0@gmail.com>
2015-05-08 16:46:40 -0700

    Moved heatmap figure generation to R script; renamed output image

---
+++ Appendix/img/Ahrendt_thesis_AppFlagella_Figs.R
@@ -0,0 +1,14 @@
+#!/usr/bin/env Rscript
+library(gplots)
+library(fastcluster)
+library(RColorBrewer)
+library(pheatmap)
+palette <- colorRampPalette(c('white','darkgreen'))(2)
+dat <- data.matrix(read.table("../dat/flagProfile_binary.tsv", sep="\t",row.names=1,header=T))
+dat <- dat[,-1]
+taxonlistID <- read.delim("../dat/taxonlistID",comment.char="#")
+ann_col <- data.frame("Taxa"=taxonlistID$Class1)
+rownames(ann_col) <- taxonlistID$Abbr
+png('flagDataHeatmap.png')
+pheatmap(dat,cluster_cols=TRUE,cluster_rows=TRUE,col=palette,clustering_method="complete",annotation_col=ann_col)
+dev.off()
