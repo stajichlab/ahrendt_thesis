753bb600a5ec8be5ecd3439677b711c4f7384367
Steven Ahrendt <sahrendt@gmail.com>
2015-05-16 12:08:10 -0700

    Making heatmap easier to read

--- Appendix/img/Ahrendt_thesis_AppFlagella_Figs.R
+++ Appendix/img/Ahrendt_thesis_AppFlagella_Figs.R
@@ -4,11 +4,16 @@ library(fastcluster)
 library(RColorBrewer)
 library(pheatmap)
 palette <- colorRampPalette(c('white','darkgreen'))(2)
-dat <- data.matrix(read.table("../dat/flagProfile_binary.tsv", sep="\t",row.names=1,header=T))
-dat <- dat[,-1]
+dat <- data.matrix(read.table("../dat/fP_rB_trimmed.tsv", sep="\t",row.names=1,header=T))
 taxonlistID <- read.delim("../dat/taxonlistID",comment.char="#")
 ann_col <- data.frame("Taxa"=taxonlistID$Class1)
 rownames(ann_col) <- taxonlistID$Abbr
-png('flagDataHeatmap.png')
-pheatmap(dat,cluster_cols=TRUE,cluster_rows=TRUE,col=palette,clustering_method="complete",annotation_col=ann_col,legend=F)
+categories <- data.frame(t(read.delim("../dat/flagProfile_Categories.tsv",row.names=1)))
+ann_row <- data.frame("Cat"=categories$Category)
+rownames(ann_row) <- rownames(categories)
+png('flagDataHeatmap.png',width=1000,height=1000)
+pheatmap(dat,cluster_cols=FALSE,cluster_rows=TRUE,col=palette,clustering_method="complete",
+         annotation_col=ann_col,
+         annotation_row=ann_row,
+         legend=F,show_rownames=F)
 dev.off()
