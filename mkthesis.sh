2ad62aaebf78fe31bfe935a499e5c6588da59751
Steven Ahrendt <sahrendt@gmail.com>
2015-07-01 17:36:25 -0700

    Ignore a lot of LaTeX output

--- mkthesis.sh
+++ mkthesis.sh
@@ -12,7 +12,9 @@ if [ -e $AUX ]
 then
   rm $AUX
 fi
-pdflatex $TEX
-bibtex $AUX
-pdflatex $TEX
-pdflatex $TEX
+
+# updated from http://tex.stackexchange.com/questions/27878/pdflatex-bash-script-to-supress-all-output-except-error-messages
+pdflatex -shell-escape -interaction=nonstopmode -file-line-error $TEX | grep -i ".*:[0-9]*:.*\|warning" > .firstpass.log
+bibtex -terse $AUX > .bibtex.log
+pdflatex -shell-escape -interaction=nonstopmode -file-line-error $TEX | grep -i ".*:[0-9]*:.*\|warning" > .secondpass.log
+pdflatex -shell-escape -interaction=nonstopmode -file-line-error $TEX | grep -i ".*:[0-9]*:.*\|warning"
